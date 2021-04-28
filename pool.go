e3d76e77d461c962fade53c6c5193f0a1e6524b2
amogh <ayermalkar99@gmail.com>
2021-04-28 18:34:32 +0530

    batch manager

--- pool.go
+++ pool.go
@@ -1,18 +1,22 @@
 package kessaku
 
 import (
+	"context"
 	"errors"
 	"os"
+	"sync"
 
 	log "github.com/sirupsen/logrus"
 )
 
 type Pool struct {
-	AtCapacity int
-	Opts       *Options
-	cache      *cache
-	// TODO: implement TaskCache
+	AtCapacity   int
+	Opts         *Options
+	cache        *cache
+	batchManager *Batch
 }
+
+var initBatchWorker sync.Once
 
 func NewPool(options ...OptionSetter) (*Pool, error) {
 	log.SetOutput(os.Stdout)
@@ -27,6 +31,10 @@ func NewPool(options ...OptionSetter) (*Pool, error) {
 }
 
 func (p *Pool) Submit(task func()) error {
+	// process order respecting bactched jobs
+	if p.Opts.WithBatch && !p.batchManager.processor.isInactive {
+		p.batchProcessor(task)
+	}
 	// get from cache
 	w := p.cache.Get()
 	// use the worker thats free
@@ -56,4 +64,18 @@ func (p *Pool) spawnWorkerAndUpdateCapacity(w *worker, task func()) {
 
 func (p *Pool) RunningWorkers() int {
 	return p.AtCapacity
+}
+
+func (p *Pool) batchProcessor(task func()) {
+	if p.batchManager.stopper == nil {
+		ctx := context.Background()
+		ctx, stopBatchedWorker := context.WithCancel(ctx)
+		p.batchManager.stopper = stopBatchedWorker
+		p.batchManager.processor = &batchWorker{}
+		p.batchManager.processor.tasks = make(chan func())
+		go p.batchManager.processor.batch(ctx)
+		p.batchManager.processor.tasks <- task
+	} else {
+		p.batchManager.processor.tasks <- task
+	}
 }
